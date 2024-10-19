90732e359fcbbb856117c33d63b59709aaed4abd
Lucas Cunha <lucas71cunha@gmail.com>
2024-10-18 23:46:09 -0300

    Testando regras de negócio

--- src/test/java/br/com/alura/adopet/ValidacaoPetDisponivelTest.java
+++ src/test/java/br/com/alura/adopet/ValidacaoPetDisponivelTest.java
@@ -1,25 +1,40 @@
 package br.com.alura.adopet;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
 import br.com.alura.adopet.api.validacoes.ValidacaoPetDisponivel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ValidacaoPetDisponivelTest {
+
+	@InjectMocks
+	ValidacaoPetDisponivel validacao;
+
+	@Mock
+	private PetRepository petRepository;
+
+	@Mock
+	private Pet pet;
+
+	@Mock
+	private SolicitacaoAdocaoDto dto;
 
 	@Test
 	void deveriaPermitirSolicitacaoDeAdocaoPet() {
 
 		// ARRANGE
-		SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(
-				7l,
-				2l,
-				"Motivo qualquer"
-		);
+		BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+		BDDMockito.given(pet.getAdotado()).willReturn(false);
 
-		ValidacaoPetDisponivel validacao = new ValidacaoPetDisponivel();
 
 		//ASSERT + ACT
 		Assertions.assertDoesNotThrow(() -> validacao.validar(dto));
